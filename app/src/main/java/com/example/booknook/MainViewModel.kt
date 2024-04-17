56bae3be67068e05263b6271105fc2906bc17819
unknown <alannaphung816@gmail.com>
2024-04-17 02:08:47 -0500

    integrated display name and bio with database

--- app/src/main/java/com/example/booknook/MainViewModel.kt
+++ app/src/main/java/com/example/booknook/MainViewModel.kt
@@ -20,7 +20,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainViewModel: ViewModel() {
-    private val apiKey = "awfn9Y2jBFJD5gpyeqTjbrK9flrjlBAn"
+    private val apiKey = ""
     private val username = MutableLiveData<String>()
     private val dbViewModel = DatabaseViewModel()
 
